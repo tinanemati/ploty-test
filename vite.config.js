ea69797aa819f738fcc2e711f78ddf3810793e8f
tinanemati <tinanemati.tina@gmail.com>
2024-01-31 00:52:10 -0800

    optimized bundle size with rollup and split vendor chunk

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { visualizer } from 'rollup-plugin-visualizer';
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), splitVendorChunkPlugin(), visualizer()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('react-plotly.js')) {
+            return 'plotly-react';
+          } else if (id.includes('plotly.js-cartesian-dist-min')) {
+            return 'plotly-min';
+          } else if (id.includes('@ag-grid-community/react')) {
+            return 'agGrid-react';
+          } else if (id.includes('@ag-grid-community/client-side-row-model') || id.includes('@ag-grid-community/styles')) {
+            return 'agGrid-community';
+          } else if (id.includes('@ag-grid-community/core/dist/esm/es6')) {
+            return 'agGrid-core-es6';
+          }
+        }
+      },
+    },
+  },
+});
